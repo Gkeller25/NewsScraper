4aad102a8382227bddfedcc6540913e7c7bf8839
Gkeller25 <kellergaret@outlook.com>
2018-08-27 21:47:28 -0600

    changed server to fix heroku error

--- server.js
+++ server.js
@@ -1,11 +1,10 @@
 var express = require("express");
-var mongojs = require("mongojs");
 var request = require("request");
 var cheerio = require("cheerio");
 var bodyParser = require("body-parser");
 var mongoose = require("mongoose");
 var exphbs = require("express-handlebars");
-var rp = require('request-promise');
+
 
 var db = require("./models");
 
@@ -31,13 +30,14 @@ var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines
 mongoose.Promise = Promise;
 mongoose.connect(MONGODB_URI);
 
-request("https://www.ksl.com/", function (err, res, html) {
 
-    // Load the html body from request into cheerio
-    var $ = cheerio.load(html);
 
 
     app.get("/", function (req, res) {
+        request("https://www.ksl.com/", function (err, res, html) {
+
+    // Load the html body from request into cheerio
+    var $ = cheerio.load(html);
         // Find all results from the scrapedData collection in the db
         $(".headline").each(function (i, element) {
             // Save the text and href of each link enclosed in the current element
@@ -51,16 +51,18 @@ request("https://www.ksl.com/", function (err, res, html) {
             var result = {};
             result.title = $(this).find("a").text();
             result.link = $(this).find("a").attr("href");
+            result.link =  "https://www.ksl.com" + result.link;
             result.summary = $(this).find("h5").text();
             result.image = $(this).siblings(".image_box").find("img").attr("data-srcset");
             result.posted = fullPostDate;
             db.Article.create(result).then(function (dbArticle) {
             })
-   
+
         })
-        res.render("empty");
+        
 
     });
+    res.render("empty");
 });
 
 app.get("/new", function (req, res) {
@@ -99,62 +101,62 @@ app.get("/all", function (req, res) {
         }
         // If there are no errors, send the data to the browser as json
         else {
-            res.render("index", 
-            {examples: found});
+            res.render("index",
+                { examples: found });
         }
     });
 });
 // Route for grabbing a specific Article by id, populate it with it's note
-app.get("/article/:id", function(req, res) {
+app.get("/article/:id", function (req, res) {
     // Using the id passed in the id parameter, prepare a query that finds the matching one in our db...
     db.Article.findOne({ _id: req.params.id })
-      // ..and populate all of the notes associated with it
-      .populate("note")
-      .then(function(dbArticle) {
-        // If we were able to successfully find an Article with the given id, send it back to the client
-        res.json(dbArticle);
-      })
-      .catch(function(err) {
-        // If an error occurred, send it to the client
-        res.json(err);
-      });
-  });
+        // ..and populate all of the notes associated with it
+        .populate("note")
+        .then(function (dbArticle) {
+            // If we were able to successfully find an Article with the given id, send it back to the client
+            res.json(dbArticle);
+        })
+        .catch(function (err) {
+            // If an error occurred, send it to the client
+            res.json(err);
+        });
+});
 
-  app.delete("/delete/:id", function(req, res) {
+app.delete("/delete/:id", function (req, res) {
     // Using the id passed in the id parameter, prepare a query that finds the matching one in our db...
     db.Note.findOneAndDelete({ _id: req.params.id })
-      // ..and populate all of the notes associated with it
-      
-      .then(function(dbNote) {
-        // If we were able to successfully find an Article with the given id, send it back to the client
-        res.json(dbNote);
-      })
-      .catch(function(err) {
-        // If an error occurred, send it to the client
-        res.json(err);
-      });
-  });
+        // ..and populate all of the notes associated with it
 
-  // Route for saving/updating an Article's associated Note
-  app.post("/article/:id", function(req, res) {
+        .then(function (dbNote) {
+            // If we were able to successfully find an Article with the given id, send it back to the client
+            res.json(dbNote);
+        })
+        .catch(function (err) {
+            // If an error occurred, send it to the client
+            res.json(err);
+        });
+});
+
+// Route for saving/updating an Article's associated Note
+app.post("/article/:id", function (req, res) {
     // Create a new note and pass the req.body to the entry
     db.Note.create(req.body)
-      .then(function(dbNote) {
-        // If a Note was created successfully, find one Article with an `_id` equal to `req.params.id`. Update the Article to be associated with the new Note
-        // { new: true } tells the query that we want it to return the updated User -- it returns the original by default
-        // Since our mongoose query returns a promise, we can chain another `.then` which receives the result of the query
-        return db.Article.findOneAndUpdate({ _id: req.params.id }, { $push: { note: dbNote._id } }, { new: false });
-      })
-      .then(function(dbArticle) {
-        // If we were able to successfully update an Article, send it back to the client
-        
-        res.json(dbArticle);
-      })
-      .catch(function(err) {
-        // If an error occurred, send it to the client
-        res.json(err);
-      });
-  });
+        .then(function (dbNote) {
+            // If a Note was created successfully, find one Article with an `_id` equal to `req.params.id`. Update the Article to be associated with the new Note
+            // { new: true } tells the query that we want it to return the updated User -- it returns the original by default
+            // Since our mongoose query returns a promise, we can chain another `.then` which receives the result of the query
+            return db.Article.findOneAndUpdate({ _id: req.params.id }, { $push: { note: dbNote._id } }, { new: false });
+        })
+        .then(function (dbArticle) {
+            // If we were able to successfully update an Article, send it back to the client
+
+            res.json(dbArticle);
+        })
+        .catch(function (err) {
+            // If an error occurred, send it to the client
+            res.json(err);
+        });
+});
 
 
 // Start the server
